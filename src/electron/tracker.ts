3ac8a0073f6363104b37ba947200f1bd21907b1b
Paul Wipper <wipper@hm.edu>
2025-10-17 23:45:47 +0200

    Add auto updating time of apps

--- src/electron/tracker.ts
+++ src/electron/tracker.ts
@@ -1,6 +1,7 @@
 import psList from "ps-list";
 import { getApps } from "./database/dbController.js";
 import { Application } from "./types.js";
+import { updateApp } from "./database/dbController.js";
 
 const POLLING_INTERVAL = 5000;
 
@@ -17,14 +18,38 @@ export function pollApplications() {
 
         for(let i = 0; i < data.length; i++){
             if(dbAppsNames.indexOf(data[i].name.toLowerCase()) > -1){
-                console.log("FOUND: ", data[i].name)
+                const appIndex = getIndexOfApp(data[i].name, dbApps)
+                if(appIndex){
+                    const app = dbApps[appIndex]
+                    console.log(`FOUND: ${app.name} at Index ${appIndex}`)
+                    const updatedApp: Application = {
+                        id: app.id,
+                        name: app.name,
+                        nickname: app.nickname,
+                        path: app.path,
+                        totalActive: app.totalActive + 5,
+                        lastActive: new Date().toISOString(),
+                        firstActive: app.firstActive === null ? new Date().toISOString() : app.firstActive 
+                    }
+                    updateApp(updatedApp)
+                }else{
+                    console.log(`FOUND: ${data[i].name} but could not find corresponding Index.`)
+                }
             }
         }
-
     }, POLLING_INTERVAL);
 }
 
 async function getApplications() {
     const data = await psList()
     return data
+}
+
+function getIndexOfApp(name: string, dbApps: Application[]){
+    for(let i = 0; i < dbApps.length; i++){
+        if(name.toLowerCase() === dbApps[i].name){
+            return i
+        }
+    }
+    return null
 }
