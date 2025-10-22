89f7f97cbcc5c464a3a6ba282d244f095ad54f2a
oltoit <166733190+oltoit@users.noreply.github.com>
2025-10-22 14:27:57 +0200

    changed Print class to Print namespace

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -2,17 +2,17 @@ export function isDev(): boolean {
     return process.env.NODE_ENV === "development";
 }
 
-export class Print {
-    static info(msg: string) {
+export namespace Print {
+    export function info(msg: string) {
         console.log(`\x1b[47m INFO  \x1b[0m \x1b[37m${msg}\x1b[0m`)
     }    
-    static ok(msg: string) {
+    export function ok(msg: string) {
         console.log(`\x1b[42m OK    \x1b[0m \x1b[32m${msg}\x1b[0m`)
     }
-    static warn(msg: string) {
+    export function warn(msg: string) {
         console.log(`\x1b[43m WARN  \x1b[0m \x1b[33m${msg}\x1b[0m`)
     }    
-    static error(msg: string) {
+    export function error(msg: string) {
         console.log(`\x1b[41m ERROR \x1b[0m \x1b[31m${msg}\x1b[0m`)
     }
 }
